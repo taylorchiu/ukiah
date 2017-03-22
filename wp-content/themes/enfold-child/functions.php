1b1b5dc42bd1cdae7494ec3f4ba1c2ca1b431c55
Taylor Chiu <taylor.chiu@zozi.com>
2017-03-21 18:51:01 -0700

    Formcraft and Cloudinary styles take precedence over Enfold

--- wp-content/themes/enfold-child/functions.php
+++ wp-content/themes/enfold-child/functions.php
@@ -6,7 +6,9 @@ function enqueue_parent_styles() {
   $template_url 		= get_template_directory_uri();
   $child_theme_url 	= get_stylesheet_directory_uri();
 
+  // Not sure why the child style.css has to live on the root or it won't override the parent style.css
   wp_enqueue_style( 'parent-style', $template_url.'/style.css' );
+  wp_enqueue_style( 'uploader_widget', $child_theme_url.'/css/uploader_widget.css' );
 
   // manually add image_uploader script and cloudinary, which will launch the cloudinary widget
   wp_register_script( 'cloudinary', 'https:////widget.cloudinary.com/global/all.js', null, null, true );
