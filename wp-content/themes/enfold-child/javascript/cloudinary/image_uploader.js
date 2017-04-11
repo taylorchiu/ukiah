3fcc748985576bfe4cca65fac1584efe8e2ed93d
Taylor Chiu <taylor.chiu@zozi.com>
2017-04-11 13:53:20 -0700

    Refactor image details toggle and only one open at a time

--- wp-content/themes/enfold-child/javascript/cloudinary/image_uploader.js
+++ wp-content/themes/enfold-child/javascript/cloudinary/image_uploader.js
@@ -4,40 +4,38 @@ jQuery(document).ready(function($) {
 
   mountUploader(jQuery('.custom-form__add_new_image.empty'));
   listenForCloudinarySuccess();
-  toggleImageDetails();
+  listenForDetailsClick();
   setDropdownOptions();
   updateForm();
 
-  function toggleImageDetails() {
+  function listenForDetailsClick() {
     jQuery('.edit-details-button').click(function(){
-      jQuery(this).siblings('.image-details__form').toggle();
-      setDetailsButtonText(this);
+      toggleImageDetails(this);
+      closeOtherDetails(this);
     });
   }
 
-  function setDetailsButtonText(button) {
+  function toggleImageDetails(button) {
+    var form = jQuery(button).siblings('.image-details__form');
     var text = jQuery(button).text();
-    if(text == '+ Add details'){
-      jQuery(button).text('- Hide details');
-    }else if(text == '+ Edit details'){
-      jQuery(button).text('- Hide details');
-    }else if(text == '- Hide details'){
-      // check if any inputs have contents
-      var inputs = jQuery(button).siblings('.image-details__form').find('.image-details-input');
-      inputs.each(function(index, el){
-        if(jQuery(el).val()){
-          jQuery(button).text('+ Edit details');
-          return false;
-        }else{
-          jQuery(button).text('+ Add details');
-        }
-      })
+    if(text == '+ details'){
+      jQuery(button).text('- details');
+    }else if(text == '- details'){
+      jQuery(button).text('+ details');
     }
+    jQuery(button).toggleClass('open');
+    form.toggleClass('open').toggle();
+  }
+
+  function closeOtherDetails(button) {
+    var otherButtons = jQuery('.edit-details-button.open').not(button);
+    jQuery.each(otherButtons, function(index, el){
+      toggleImageDetails(el);
+    })
   }
 
   function setDropdownOptions() {
     // IMPORTANT: label must match the Gravity Forms field label!
-    // TODO: how to make less breakable??
     var dropdowns = [{ id: 'paper', label: 'paper_options' }, { id: 'border', label: 'border_options' }];
     dropdowns.forEach(function(dropdown) {
       // get input id
@@ -127,13 +125,13 @@ jQuery(document).ready(function($) {
     var images = [];
     jQuery.each(IMAGE_DETAILS, function(publicId, detailsObj) {
       var image = [];
-      image.push("\n =========================");
+      image.push("=========================");
       image.push("Cloudinary Image ID: " + publicId);
       jQuery.each(detailsObj, function(key, val){
         key = key.charAt(0).toUpperCase() + key.slice(1);
         image.push(key + ": " + val + ", ");
       });
-      image.push("========================= \n");
+      image.push("=========================\n");
       images.push(image.join("\n"));
     })
     return images.join("\n");
