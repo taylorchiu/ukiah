51aa370692d2891f7b59a2acae7a2ce12490d4f6
Taylor Chiu <taylor.chiu@zozi.com>
2017-04-26 12:41:24 -0700

    Add WP-SCSS plugin and configure

--- wp-content/themes/enfold-child/functions.php
+++ wp-content/themes/enfold-child/functions.php
@@ -1,15 +1,18 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+define('WP_SCSS_ALWAYS_RECOMPILE', true);
 
 function enqueue_parent_styles() {
   $template_url 		= get_template_directory_uri();
   $child_theme_url 	= get_stylesheet_directory_uri();
 
+  // WP-SCSS plugin automatically compiles all css files
   // Not sure why the child style.css has to live on the root or it won't override the parent style.css
-  wp_enqueue_style( 'parent-style', $template_url.'/style.css' );
-  wp_enqueue_style( 'uploader_widget', $child_theme_url.'/css/uploader_widget.css' );
-  wp_enqueue_style( 'form', $child_theme_url.'/css/form.css' );
+  // wp_enqueue_style( 'parent-style', $template_url.'/style.css' );
+  // wp_enqueue_style( 'uploader_widget', $child_theme_url.'/css/uploader_widget.css' );
+  // wp_enqueue_style( 'form', $child_theme_url.'/scss/form.scss' );
 
   // manually add image_uploader script and cloudinary, which will launch the cloudinary widget
   wp_register_script( 'cloudinary', 'https://widget.cloudinary.com/global/all.js', null, null, true );
